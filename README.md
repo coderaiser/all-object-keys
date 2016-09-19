785e776bf80d0c7799519393dd12520af6a90551
coderaiser <coderaiser@cloudcmd.io>
2016-09-19 15:46:21 +0300

    feature(all-object-keys) add

---
+++ README.md
@@ -0,0 +1,66 @@
+# all-object-keys [![License][LicenseIMGURL]][LicenseURL] [![NPM version][NPMIMGURL]][NPMURL] [![Dependency Status][DependencyStatusIMGURL]][DependencyStatusURL] [![Build Status][BuildStatusIMGURL]][BuildStatusURL] [![Coverage Status][CoverageIMGURL]][CoverageURL]
+
+Get all keys of object. Use [jessy](https://github.com/coderaiser/jessy "jessy") to get value.
+Use [nessy](https://github.com/coderaiser/nessy "nessy") to set value.
+
+## Install
+
+`npm i all-object-keys --save`
+
+## Hot to use?
+
+```js
+const keys = require('all-object-keys');
+
+keys({
+    universal: true,
+    hello: {
+        world: {
+            'could be used in browser as well'
+        }
+    }
+});
+
+// returns
+['universal', 'hello.world']
+```
+## Environments
+
+In old `node.js` environments that supports `es5` only, `all-object-keys` could be used with:
+
+```js
+var keys = require('all-object-keys/legacy');
+```
+
+I browser:
+
+```html
+<script src="dist/all-object-keys.js"></script>
+<script>
+    allObjectKeys('.', {
+        could: {
+            be: 'used'
+        }
+    });
+    
+    // returns
+    ['could.be']
+</script>
+```
+
+## License
+
+MIT
+
+[NPMIMGURL]:                https://img.shields.io/npm/v/all-object-keys.svg?style=flat
+[BuildStatusIMGURL]:        https://img.shields.io/travis/coderaiser/all-object-keys/master.svg?style=flat
+[DependencyStatusIMGURL]:   https://img.shields.io/gemnasium/coderaiser/all-object-keys.svg?style=flat
+[LicenseIMGURL]:            https://img.shields.io/badge/license-MIT-317BF9.svg?style=flat
+[NPMURL]:                   https://npmjs.org/package/all-object-keys "npm"
+[BuildStatusURL]:           https://travis-ci.org/coderaiser/all-object-keys  "Build Status"
+[DependencyStatusURL]:      https://gemnasium.com/coderaiser/all-object-keys "Dependency Status"
+[LicenseURL]:               https://tldrlegal.com/license/mit-license "MIT License"
+
+[CoverageURL]:              https://coveralls.io/github/coderaiser/all-object-keys?branch=master
+[CoverageIMGURL]:           https://coveralls.io/repos/coderaiser/all-object-keys/badge.svg?branch=master&service=github
+
---
+++ test/all-object-keys.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const test = require('tape');
+const keys = require('..');
+
+test('arguments: no', (t) => {
+    t.throws(keys, 'should throw when no args');
+    t.end();
+});
+
+test('arguments: no delimiter', (t) => {
+    const result = keys({
+        hello: {
+            world: true
+        }
+    });
+    
+    const expect = ['hello.world'];
+    
+    t.deepEqual(result, expect, 'should use default delimiter when only object provided');
+    t.end();
+});
+
+test('arguments: divider not string', (t) => {
+    const fn = () => keys(1, {});
+    
+    t.throws(fn, /divider should be a string/, 'should throw when divider not string');
+    t.end();
+});
+
+test('result: should return array', (t) => {
+    const expect = [
+        'config_plugins',
+        'config_compile_client',
+        'config_compile_client_min',
+        'config_compile_vendor',
+        'config_compile_vendor_min'
+    ];
+    
+    const result = keys('_', {
+        config: {
+            plugins: 'some',
+            compile: {
+                client: 'hello',
+                client_min: 'world',
+                vendor: '31337',
+                vendor_min: '1337'
+            }
+        }
+    });
+    
+    t.deepEqual(result, expect, 'should get key pathes');
+    t.end();
+});
+
+test('result: should return array: when value is null ', (t) => {
+    const expect = [
+        'config_plugins',
+    ];
+    
+    const result = keys('_', {
+        config: {
+            plugins: null
+        }
+    });
+    
+    t.deepEqual(result, expect, 'should get key pathes');
+    t.end();
+});
+
+test('result: should return array: when value is null ', (t) => {
+    const expect = [
+        'config_plugins',
+    ];
+    
+    const result = keys('_', {
+        config: {
+            plugins: null
+        }
+    });
+    
+    t.deepEqual(result, expect, 'should get key pathes');
+    t.end();
+});
+
+test('result: should return array: when value is empty object', (t) => {
+    const expect = [
+        'config_plugins',
+    ];
+    
+    const result = keys('_', {
+        config: {
+            plugins: {}
+        }
+    });
+    
+    t.deepEqual(result, expect, 'should get key pathes');
+    t.end();
+});
