278bfd992f6c4c332c0ca697a27ff90512e69a71
Sergey Padurets <sergey@padurets.com>
2016-12-22 10:52:09 +0300

    chore(package) v2.0.0

--- dist/all-object-keys.es6.js
+++ dist/all-object-keys.es6.js
@@ -1,6 +1,12 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.allObjectKeys = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"all-object-keys":[function(require,module,exports){
 'use strict';
 
+var defaultOptions = {
+    divider: '.',
+    iPrefix: '',
+    iSuffix: ''
+}
+
 const isObject = (obj) => {
     return typeof obj === 'object';
 };
@@ -9,41 +15,59 @@ const isEmptyObject = (obj) => {
     return !Object.keys(obj).length;
 }
 
-module.exports = (divider, obj) => {
-    if (!obj) {
-        obj = divider;
-        divider = '.';
+const isNumber = (n) => {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+module.exports = (userOptions, obj) => {
+    var options = defaultOptions;
+
+    if(typeof userOptions === 'object'){
+        if(!obj){
+            obj = userOptions;
+        }else{
+            options = Object.assign(options, userOptions);
+        }
     }
-    
-    check(divider, obj);
-    
-    return keys(divider, '', obj);
+
+    check(options.divider, obj);
+
+    return keys(options, '', obj);
 };
 
 function check(divider, obj) {
-    if (typeof divider !== 'string')
+    if (typeof divider !== 'string'){
         throw Error('divider should be a string!');
-    
-    if (typeof obj !== 'object')
+    }
+
+    if (typeof obj !== 'object'){
         throw Error('obj should be an object!');
+    }
 }
 
-function keys(divider, path, obj, array) {
-    if (!array)
+function keys(options, path, obj, array) {
+    var divider = options.divider;
+
+    if (!array){
         array = [];
-    
-    if (!obj || !isObject(obj) || isEmptyObject(obj))
+    }
+
+    if (!obj || !isObject(obj) || isEmptyObject(obj)){
         array.push(path);
-    else
-        Object.keys(obj).forEach((key) => {
-            const current = !path ?
-                key
-                :
-                [path, key].join(divider);
-             
-            keys(divider, current, obj[key], array);
+    }else{
+        Object.keys(obj).forEach((origKey) => {
+            var key = origKey;
+            if(isNumber(key) && options.iPrefix){
+                divider = '';
+                key = options.iPrefix+key+options.iSuffix;
+            }
+
+            const current = !path ? key : [path, key].join(divider);
+
+            keys(options, current, obj[origKey], array);
         });
-    
+    }
+
     return array;
 }
 
