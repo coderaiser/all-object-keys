768f40ca71c38b94f31fec03b24a9ac8a28dadf3
coderiaser <coderaiser@cloudcmd.io>
2025-02-23 16:15:08 +0200

    chore: all-object-keys: rm unused

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,3 @@
 import {safeAlign} from 'eslint-plugin-putout';
-import {createESLintConfig} from '@putout/eslint-flat';
 
-export default createESLintConfig([
-    safeAlign, {
-        rules: {
-            'node/no-unsupported-features/node-builtins': 'off',
-        },
-    },
-]);
+export default safeAlign;
